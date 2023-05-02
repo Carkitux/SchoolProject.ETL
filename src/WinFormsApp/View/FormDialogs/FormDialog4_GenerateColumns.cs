19bbeb29122e0dec57f0cc559b0c274e11cfa642
Carsten Lehrmann <99147882+Carkitux@users.noreply.github.com>
2023-05-02 09:58:14 +0200

    Add FormDialog

---
+++ src/WinFormsApp/View/FormDialogs/FormDialog4_GenerateColumns.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SchoolProject.ETL.UI.WinFormsApp.View.FormDialogs
+{
+    public partial class FormDialog4_GenerateColumns : Form
+    {
+        public FormDialog4_GenerateColumns()
+        {
+            InitializeComponent();
+        }
+    }
+}
